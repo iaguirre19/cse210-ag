fe5f3e11b732c8e3c12cdac83e944ed2f828ae42
Iran Aguirre <iran.agui1993@gmail.com>
2025-10-27 22:50:32 -0600

    Add: Excercise 01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,32 @@
+// For this exercise I try to validate that the user does not enter empty names, using a do-while loop.
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        string firstName;
+        string lastName;
+
+        do
+        {
+            Console.Write("What is your first name? ");
+            firstName = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(firstName))
+            {
+                Console.WriteLine("First name cannot be empty. Please try again.");
+            }
+        } while (string.IsNullOrWhiteSpace(firstName));
+
+        do
+        {
+            Console.Write("What is your last name? ");
+            lastName = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(lastName))
+            {
+                Console.WriteLine("Last name cannot be empty. Please try again.");
+            }
+        } while (string.IsNullOrWhiteSpace(lastName));
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
