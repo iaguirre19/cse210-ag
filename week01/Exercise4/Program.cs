f6d3db8252a2fa06ce74c8ec7f7c1d2221e27d16
Iran Aguirre <iran.agui1993@gmail.com>
2025-10-30 21:15:23 -0600

    Add: Implement number input, sum, average, and largest number calculation

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int input;
+        int sum = 0;
+        int largest = 0;
+
+
+        do
+        {
+            Console.Write("Enter a number (or type '0' to finish): ");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+
+        if(numbers.Count > 0)
+        {
+            
+        foreach (int number in numbers)
+        {
+            sum += number;
+
+            if (number > largest)
+            {
+                largest = number;
+            }
+
+
+
+        }
+        double average = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average:F2}"); // Formato con 2 decimales
+            Console.WriteLine($"The largest number is: {largest}");
+        } else
+        {
+            Console.WriteLine("Not numbers entered");
+        }
+
     }
 }
